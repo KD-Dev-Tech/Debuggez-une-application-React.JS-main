398f1b7f9330726473fe353b605535657e8d0848
KD-Dev-Tech <08.kdlan@gmail.com>
2024-07-22 15:10:01 +0200

    Bug supprimer sur Form / Slider / Select

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -16,8 +16,8 @@ const Select = ({
   const [value, setValue] = useState();
   const [collapsed, setCollapsed] = useState(true);
   const changeValue = (newValue) => {
-    onChange();
-    setValue(newValue);
+    onChange(newValue);
+    setValue(newValue); // Ajout de la valeur newValue pour la sélection
     setCollapsed(newValue);
   };
   return (
